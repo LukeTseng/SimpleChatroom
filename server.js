a4fa40ab2d13eb0dc910af90c31e873496409e3e
Luke <luke4775@gmail.com>
2015-07-08 00:31:27 +0800

    Temp version: finish socket.io

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var http = require('http');
+var socket = require('./socket.js')
 var app = express();
-var io = require('socket.io').listen.(app);
+
 var port = 5000;
 
+/* app configuration */
 app.use(express.static(__dirname + '/public'));
 
+var server = http.createServer(app);
+var io = require('socket.io')(server);
 
 /* routing */
 app.get('/', function(req, res) {
@@ -12,9 +17,22 @@ app.get('/', function(req, res) {
 	res.sendFile("index.html")
 });
 
+/* socket io */
+// io.on('connection', function(socket){
+//   console.log('A new connection')
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//     console.log(msg)
+//   });
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
+io.on('connection', socket)
 
 
-
-app.listen(port);
-console.log("App listen at port " + port)
+server.listen(port, function(){
+	console.log("APP listen at port" + port);
+});
 
